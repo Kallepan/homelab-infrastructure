300980baa29d16b34a418e38230eb4e167109697
Kajeepan Kandeepan <kajeepan.kandeepan@gmail.com>
2025-12-11 23:00:00 +0100

    feat: update Argo CD chart version to 9.1.7

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -12,7 +12,7 @@ resource "helm_release" "argocd" {
   chart      = "argo-cd"
 
   namespace = kubernetes_namespace_v1.argocd.metadata[0].name
-  version = "9.1.6"
+  version = "9.1.7"
   wait = true
 
   values = [
